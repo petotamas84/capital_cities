64432b4ef6112c59e40fe40aaceac064b55f856c
Pető Tamás <petotamas0@gmail.com>
2024-01-26 12:31:16 +0000

    initial commit

---
+++ capitals/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Capital(models.Model):
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.city}, {self.country}"
+
+    def get_absolute_url(self):
+        return reverse("home")
